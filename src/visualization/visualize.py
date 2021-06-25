6ff080294e8c98d732895cf896b6ef2e77b7381b
AleksU <avoicikas@gmail.com>
2021-06-25 23:16:01 +0300

    visualization fixed

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -43,20 +43,24 @@ def plot_psds():
             ax[0][1].set_title(f'{ifile.stem} {group}')
             profile_max=profile.max()[0]
             profile_idxmax=profile.idxmax()[0]
+            profile_values_mean = pd.DataFrame({'igf':[profile_idxmax],'igf_value':[profile_max]})
             ax[0][1].text(profile_idxmax,profile_max-.1,f'{int(profile_idxmax)} Hz\n {profile_max:.2}')
             profile=datt[datt.stimfreq==datt.ifreq].pivot_table(values='spectra',index=['ifreq'], columns='ichan')
             profile_values = pd.DataFrame({'igf':profile.idxmax(),'igf_value':profile.max()})
             chnames = pickle.load(open('chnames.p','rb'))
             pos=pickle.load(open('info.p','rb'))
-            im=mne.viz.plot_topomap(profile_values.igf,pos,axes=ax[1][0],cmap=cm.viridis,contours=0,vmin=35)
+            im=mne.viz.plot_topomap(profile_values.igf,pos,axes=ax[1][0],cmap=cm.viridis,contours=0,vmin=35,show=False)
             plt.colorbar(im[0],ax=ax[1][0])
-            im=mne.viz.plot_topomap(profile_values.igf_value,pos,cmap=cm.viridis, contours=0, axes=ax[1][1])
+            im=mne.viz.plot_topomap(profile_values.igf_value,pos,cmap=cm.viridis,
+                    contours=0, axes=ax[1][1],show=False)
             plt.colorbar(im[0],ax=ax[1][1])
             plt.tight_layout()
             plt.savefig(saveDir.joinpath(f'{ifile.stem}_{group}.png'))
             plt.close('all')
             save_igf(profile_values.igf, ifile.stem, 'igf', group, saveDirData)
+            save_igf_mean(profile_values_mean.igf, ifile.stem, 'igf_cmean', group, saveDirData)
             save_igf(profile_values.igf_value, ifile.stem, 'igf_value', group, saveDirData)
+            save_igf_mean(profile_values_mean.igf_value, ifile.stem, 'igf_cmean_value', group, saveDirData)
 
 def save_igf(values,  powertype, datatype, subj, saveDir):
     csvfile = saveDir.joinpath(powertype + datatype+'.csv')
@@ -68,6 +72,16 @@ def save_igf(values,  powertype, datatype, subj, saveDir):
         vt = [str(x) for x in values.values]
         vt = ','.join(vt)
         f.write(f"{subj+','+vt}"+'\n')
+
+def save_igf_mean(values,  powertype, datatype, subj, saveDir):
+    csvfile = saveDir.joinpath(powertype + datatype+'.csv')
+    if not pathlib.Path(csvfile).exists():
+        csvfile.write_text(f'subj,'+'mean_center'+'\n')
+    with open(csvfile,'a') as f:
+        vt = [str(x) for x in values.values]
+        vt = ','.join(vt)
+        f.write(f"{subj+','+vt}"+'\n')
+
 
 
 if __name__ == "__main__":
